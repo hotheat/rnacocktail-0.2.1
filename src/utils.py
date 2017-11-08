5b61c08eb9ab0703efafa40adc7bdd8c308dffea
hotheat <guoam630@outlook.com>
2017-11-08 14:27:58 +0800

    11.08-push

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import logging
+import os
+import time
+
+logger = logging.getLogger(__name__)
+
+
+def create_dirs(dirlist):
+    for dirname in dirlist:
+        if not os.path.isdir(dirname):
+            logger.info("Creating directory %s" % (dirname))
+            os.makedirs(dirname)
+
